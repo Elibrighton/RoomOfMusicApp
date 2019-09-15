fa948481119ac89f8a11c85a8b3e43c97e666114
Eli Brighton <eli.brighton@gmail.com>
2019-09-15 10:35:30 +1000

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import About from './About'
+import Contact from './Contact';
+import Home from './Home';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route
+        path="/about"
+        component={About}
+      />
+      <Route path="/contact" component={Contact} />
+    </Switch>
   );
 }
 
