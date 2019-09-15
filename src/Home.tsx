1b156739fa286b5753e46c12c09a129ccfce46c7
Eli Brighton <eli.brighton@gmail.com>
2019-09-15 10:59:20 +1000

    Add Link to navigation

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component {
     render() {
-        return <div>Home</div>
+        return (
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        )
     }
 }
